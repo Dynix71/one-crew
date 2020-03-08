4be91f601416311d72dcf5a8c3470c4167fd9aca
Tomasz Grzegorek <grzegor@hotmail.com>
2020-03-08 15:41:21 +0100

    Add folder for API and initial API project.

---
+++ api/src/Api/Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        private readonly ILogger<HelloController> _logger;
+
+        public HelloController(ILogger<HelloController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Content("Hello");
+        }
+    }
+}
